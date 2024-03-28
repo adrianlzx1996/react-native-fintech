58f000587d9e7c35c9bb66da7d105bc9f6aba877
Adrian <leongadrian36@gmail.com>
2024-03-28 22:56:23 +0800

    zustand mmkv store

---
+++ store/balanceStorage.ts
@@ -0,0 +1,33 @@
+import { zustandStorage } from '@/store/mmkv-storage'
+import { create } from 'zustand';
+import { createJSONStorage, persist } from "zustand/middleware";
+
+export interface Transaction {
+	id: string;
+	amount: number;
+	date: Date;
+	title: string;
+}
+
+export interface BalanceState {
+	transactions: Array<Transaction>;
+	runTransaction: (transaction: Transaction) => void;
+	balance: () => number;
+	clearTransaction: () => void;
+}
+
+export const useBalanceStore = create<BalanceState>()(
+	persist(
+		(set, get) => ({
+			transactions: [],
+			runTransaction: (transaction: Transaction) => {
+				set(state => ({ transactions: [...state.transactions, transaction] }))
+			},
+			balance: () => get().transactions.reduce((acc, t) => acc + t.amount, 0),
+			clearTransaction: () => set(() => ({ transactions: [] })),
+		}),
+		{
+			name: 'balance-storage',
+			storage: createJSONStorage(() => zustandStorage),
+		})
+)
