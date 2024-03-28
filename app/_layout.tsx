8c4ef72a16b3b4e12c271571e0ac9f9995c8f4fd
Adrian <leongadrian36@gmail.com>
2024-03-28 17:27:59 +0800

    auth setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,58 +1,126 @@
+import Colors from '@/constants/Colors';
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { Ionicons } from '@expo/vector-icons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
 
-import { useColorScheme } from '@/components/useColorScheme';
+import { useFonts } from 'expo-font';
+import { Link, Stack, useRouter } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import * as SecureStore from 'expo-secure-store';
+
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+const tokenCache = {
+	async getToken(key: string) {
+		try {
+			return SecureStore.getItemAsync(key);
+		} catch (error) {
+			return null;
+		}
+	},
+	async saveToken(key: string, value: string) {
+		try {
+			return SecureStore.setItemAsync(key, value);
+		} catch (error) {
+			return;
+		}
+	}
+}
 
 export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
+	// Catch any errors thrown by the Layout component.
+	ErrorBoundary,
 } from 'expo-router';
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+	// Ensure that reloading on `/modal` keeps a back button present.
+	initialRouteName: '(tabs)',
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
+const InitialLayout = () => {
+	const [loaded, error] = useFonts({
+		SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+		...FontAwesome.font,
+	});
+	const router = useRouter();
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+	// Expo Router uses Error Boundaries to catch errors in the navigation tree.
+	useEffect(() => {
+		if (error) throw error;
+	}, [error]);
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+	useEffect(() => {
+		if (loaded) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
+	if (!loaded) {
+		return null;
+	}
 
-  return <RootLayoutNav />;
+	return <Stack>
+		<Stack.Screen name="index" options={{ headerShown: false }} />
+		<Stack.Screen
+			name="register"
+			options={{
+				title: '',
+				headerBackTitle: '',
+				headerShadowVisible: false,
+				headerStyle: { backgroundColor: Colors.background },
+				headerLeft: () => (
+					<TouchableOpacity onPress={router.back}>
+						<Ionicons name="arrow-back" size={32} color={Colors.dark} />
+					</TouchableOpacity>
+				)
+			}}
+		/>
+		<Stack.Screen
+			name="login"
+			options={{
+				title: '',
+				headerBackTitle: '',
+				headerShadowVisible: false,
+				headerStyle: { backgroundColor: Colors.background },
+				headerLeft: () => (
+					<TouchableOpacity onPress={router.back}>
+						<Ionicons name="arrow-back" size={32} color={Colors.dark} />
+					</TouchableOpacity>
+				),
+				headerRight: () => (
+					<Link href={'/help'} asChild>
+						<TouchableOpacity>
+							<Ionicons name="help-circle-outline" size={32} color={Colors.dark} />
+						</TouchableOpacity>
+					</Link>
+				),
+			}}
+		/>
+
+		<Stack.Screen name="help" options={{ presentation: 'modal', title: "Help" }} />
+	</Stack>;
 }
 
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+const RootLayoutNav = () => {
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
-  );
+	return (
+		<ClerkProvider
+			tokenCache={tokenCache}
+			publishableKey={CLERK_PUBLISHABLE_KEY!}
+		>
+			<GestureHandlerRootView style={{ flex: 1 }}>
+				<StatusBar style='auto' />
+				<InitialLayout />
+			</GestureHandlerRootView>
+		</ClerkProvider>
+	);
 }
+
+export default RootLayoutNav
