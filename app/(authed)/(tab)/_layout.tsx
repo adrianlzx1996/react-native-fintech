6eedbaedd65d06ea1539a04f3297b62c8d9bd383
Adrian <leongadrian36@gmail.com>
2024-03-29 10:51:23 +0800

    bottom tab bar blur view

--- app/(authed)/(tab)/_layout.tsx
+++ app/(authed)/(tab)/_layout.tsx
@@ -1,11 +1,22 @@
 import Colors from "@/constants/Colors"
 import { FontAwesome } from "@expo/vector-icons"
-import { Tabs } from "expo-router"
+import { Tabs } from "expo-router";
+import { BlurView } from "expo-blur"
 
 const Layout = () => {
 	return (
 		<Tabs screenOptions={{
 			tabBarActiveTintColor: Colors.primary,
+			tabBarBackground: () => <BlurView intensity={100} style={{ flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.05)' }} />,
+			tabBarStyle: {
+				backgroundColor: 'transparent',
+				position: 'absolute',
+				bottom: 0,
+				left: 0,
+				right: 0,
+				elevation: 0,
+				borderTopWidth: 0,
+			}
 		}}>
 			<Tabs.Screen
 				name="home"
